838bcbb4082e2a9c6301246a71df9f405e235b12
cjeongmin <cjeongmin27@gmail.com>
2024-03-14 01:44:43 +0900

    fix: 기존 구조의 훅 제거 (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-'use client';
-
-import { MainPageWithLogin, MainPageWithoutLogin } from './pages';
-
-import { useLoginStatus } from '@/hooks';
-
 export default function Home() {
-  const loginStatus = useLoginStatus();
-
-  return <>{loginStatus ? <MainPageWithLogin /> : <MainPageWithoutLogin />}</>;
+  return <></>;
 }
