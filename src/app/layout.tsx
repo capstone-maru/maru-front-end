5b1ce64f43091c069b1f683d09102b372ff53bd4
he2e2 <dddd6368@naver.com>
2024-03-19 22:08:57 +0900

    feat: 구글 폰트 설정 (#10)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>
         <TanstackQueryProvider>
           <RecoilRootProvider>
