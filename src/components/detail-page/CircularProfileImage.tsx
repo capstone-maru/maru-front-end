59bd796817f7b782f15bc0e38ca2a325a4d515a4
cjeongmin <cjeongmin27@gmail.com>
2024-03-20 18:02:59 +0900

    feat: CircularProfileImage Percentage 추가 (#12)

--- src/components/detail-page/CircularProfileImage.tsx
+++ src/components/detail-page/CircularProfileImage.tsx
@@ -13,6 +13,30 @@ const styles = {
     background-image: ${({ $url }) => `url("${$url}")`};
     background-position: center;
   `,
+  percentage: styled.div`
+    width: 48px;
+    height: 48px;
+
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+
+    border-radius: 200px;
+    background: rgba(255, 255, 255, 0.8);
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.04);
+    color: #e15637;
+
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 24px;
+
+    top: -40%;
+    left: 65%;
+  `,
 };
 
 export function CircularProfileImage({
@@ -26,13 +50,12 @@ export function CircularProfileImage({
 }) {
   return (
     <styles.container $diameter={diameter} $url={url}>
-      <div>
-        <CircularProgressBar
-          diameter={diameter}
-          percentage={percentage}
-          strokeWidth={10}
-        />
-      </div>
+      <CircularProgressBar
+        diameter={diameter}
+        percentage={percentage}
+        strokeWidth={10}
+      />
+      <styles.percentage>{percentage}%</styles.percentage>
     </styles.container>
   );
 }
