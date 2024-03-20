af6489d3e757132b657eb02d6b4f2a294715560f
cjeongmin <cjeongmin27@gmail.com>
2024-03-21 01:39:57 +0900

    feat: proxy 설정 (#16)

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*/`,
+      },
+    ];
+  },
+  trailingSlash: false /* 만약, 후행 슬래시가 필요하다면 true, default → false */,
 };
 
 export default nextConfig;
