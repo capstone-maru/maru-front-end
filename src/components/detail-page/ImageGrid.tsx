bd338b49b82d8d32973d00e589aad8caeec2cca6
cjeongmin <cjeongmin27@gmail.com>
2024-03-26 16:19:41 +0900

    fix: build error - remove unused hook

--- src/components/detail-page/ImageGrid.tsx
+++ src/components/detail-page/ImageGrid.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 
 import { ImageIndexButton } from './ImageIndexButton';
