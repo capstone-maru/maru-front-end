358374ad5c513bd1a32de323b431866908375b1b
cjeongmin <cjeongmin27@gmail.com>
2024-03-26 16:10:16 +0900

    feat: ImageGrid 컴포넌트 수정 (#24)

--- src/components/detail-page/ImageGrid.tsx
+++ src/components/detail-page/ImageGrid.tsx
@@ -3,78 +3,51 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+import { ImageIndexButton } from './ImageIndexButton';
+
 const styles = {
-  container: styled.div<{ $imageCount: number }>`
+  container: styled.div`
+    position: relative;
+    display: flex;
+    align-items: center;
     width: 100%;
-    height: 18.75rem;
-    border-radius: 16px;
+    height: 100%;
+  `,
+  buttonContainer: styled.div`
+    position: absolute;
+
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    padding: 0.69rem;
+  `,
+  imageContainer: styled.div`
+    width: 100%;
 
     display: grid;
     grid-template-columns: 1fr 1fr;
-    grid-auto-rows: 18.75rem;
-    gap: 0.5rem;
+    grid-auto-rows: 20.625rem;
+    gap: 0.81rem;
 
-    .image-grid {
-      display: grid;
-      height: 18.75rem;
-
-      grid-template-columns: 1fr 1fr;
-      grid-template-rows: 9.125rem 9.125rem;
-      grid-auto-rows: 18.75rem;
-      gap: 0.5rem;
-
-      img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-      }
-
-      .round-top {
-        border-radius: 0 16px 0 0;
-      }
-
-      .round-bottom {
-        border-radius: 0 0 16px 0;
-      }
-    }
-
-    .thumbnail {
+    img {
       width: 100%;
       height: 100%;
+      object-fit: cover;
+    }
+
+    .left {
       border-radius: 16px 0 0 16px;
     }
-  `,
-  imageCell: styled.div<{ $url: string }>`
-    color: #000;
-    font-family: 'Noto Sans KR';
-    font-size: 1.5rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
 
-    background-position: center;
-    background-size: cover;
-    background-image: ${({ $url }) => `url("${$url}")`};
-    background-color: ${({ $url }) => ($url === 'none' ? '#c4c4c480' : '')};
-
-    div {
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
-
-      display: flex;
-      align-items: center;
-      justify-content: center;
-
-      width: 100%;
-      height: 100%;
-      background-color: rgb(255 255 255 / 30%);
-      backdrop-filter: blur(0.5rem);
-      border-radius: 0 0 16px 0;
-
-      cursor: pointer;
+    .right {
+      border-radius: 0 16px 16px 0;
     }
+  `,
+  none: styled.div`
+    width: 100%;
+    height: 100%;
+    background: var(--Gray-6, #efefef);
   `,
 };
 
@@ -82,65 +55,62 @@ interface Props {
   images: string[];
 }
 
-function ImageCell({
-  key,
-  url,
-  className,
-  restImageCount,
-}: {
-  key: string;
-  url: string;
-  className: string;
-  restImageCount: number;
-}) {
-  const hasOverlay = className.includes('overlay');
-
-  return (
-    <styles.imageCell key={key} $url={url} className={className}>
-      {hasOverlay && <div>+ {restImageCount}</div>}
-    </styles.imageCell>
-  );
-}
+const sliceImages = (images: string[]) => {
+  const result: string[][] = [];
+  for (let i = 0; i < images.length; i += 2) {
+    if (i + 1 < images.length) {
+      result.push([images[i], images[i + 1]]);
+    } else {
+      result.push([images[i], 'none']);
+    }
+  }
+  return result;
+};
 
 export function ImageGrid({ images: imagesParam }: Props) {
-  const [thumbnail] = useState<string | undefined>(imagesParam[0]);
-  const [images, setImages] = useState(imagesParam.slice(1, 5));
-  const [restImages] = useState<string[]>(imagesParam.slice(5));
+  const [images] = useState(sliceImages(imagesParam));
+  const [pageIndex, setPageIndex] = useState(0);
 
-  const applyRound = (index: number) => {
-    if (index === 1) return 'round-top';
-    if (index === 3) return 'round-bottom';
-    return '';
-  };
-
-  const applyOverlay = (index: number) => {
-    if (index === 3 && restImages.length > 0) return 'overlay';
-    return '';
-  };
-
-  useEffect(() => {
-    if (images.length < 4) {
-      setImages(
-        images.concat(
-          Array.from({ length: 4 - images.length }).map(() => 'none'),
-        ),
-      );
+  const handlePageChange = (diff: number) => {
+    if (pageIndex + diff < 0 || pageIndex + diff >= images.length) {
+      return;
     }
-  }, []);
+
+    setPageIndex(pageIndex + diff);
+  };
 
   return (
-    <styles.container $imageCount={images.length}>
-      <img alt="" src={thumbnail} className="thumbnail" />
-      <div className="image-grid">
-        {images.map((image, index) =>
-          ImageCell({
-            key: image,
-            url: image,
-            className: [applyRound(index), applyOverlay(index)].join(' '),
-            restImageCount: restImages.length,
-          }),
+    <styles.container>
+      <styles.buttonContainer>
+        <ImageIndexButton
+          disabled={pageIndex - 1 < 0}
+          direction="left"
+          onClick={() => {
+            handlePageChange(-1);
+          }}
+        />
+        <ImageIndexButton
+          disabled={pageIndex + 1 >= images.length}
+          direction="right"
+          onClick={() => {
+            handlePageChange(1);
+          }}
+        />
+      </styles.buttonContainer>
+      <styles.imageContainer>
+        {images[pageIndex].map((image, index) =>
+          image !== 'none' ? (
+            <img
+              key={image}
+              alt={image}
+              src={image}
+              className={index % 2 === 0 ? 'left' : 'right'}
+            />
+          ) : (
+            <styles.none key="none" className="right" />
+          ),
         )}
-      </div>
+      </styles.imageContainer>
     </styles.container>
   );
 }
