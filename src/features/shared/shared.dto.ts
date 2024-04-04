7f28d9a69fd21e413c50ded95549509f7b786559
cjeongmin <cjeongmin27@gmail.com>
2024-04-05 02:45:47 +0900

    fix: modify GetSharedPostDTO (#46)

--- src/features/shared/shared.dto.ts
+++ src/features/shared/shared.dto.ts
@@ -86,55 +86,57 @@ export interface GetSharedPostsDTO extends SuccessBaseDTO {
 }
 
 export interface GetSharedPostDTO extends SuccessBaseDTO {
-  id: number;
-  title: string;
-  content: string;
-  roomImages: Set<{
-    fileName: string;
-    isThumbnail: boolean;
-    order: number;
-  }>;
-  publisherAccount: {
-    memberId: string;
-    email: string;
-    nickname: string;
-    birthYear: string;
-    gender: string;
-    phoneNumber: string;
-    myCardFeatures: string[];
-    mateCardFeatures: string[];
+  data: {
+    id: number;
+    title: string;
+    content: string;
+    roomImages: Set<{
+      fileName: string;
+      isThumbnail: boolean;
+      order: number;
+    }>;
+    publisherAccount: {
+      memberId: string;
+      email: string;
+      nickname: string;
+      birthYear: string;
+      gender: string;
+      phoneNumber: string;
+      myCardFeatures: string[];
+      mateCardFeatures: string[];
+      createdAt: Date;
+      createdBy: string;
+      modifiedAt: Date;
+      modifiedBy: string;
+    };
+    roomInfo: {
+      id: number;
+      address: {
+        city: string;
+        oldAddress: string;
+        roadAddress: string;
+        detailAddress?: string;
+        stationName: string;
+        stationTime: number;
+        busStopTime: number;
+        schoolName: string;
+        schoolTime: number;
+        convenienceStortTime: number;
+      };
+      roomType: RoomType;
+      size: number;
+      numberOfRoom: number;
+      rentalType: RentalType;
+      price: number;
+      managementFee: number;
+      expectedPayment: number;
+      monthlyFee: number;
+    };
+    isScrapped: boolean;
+    scrapCount: number;
     createdAt: Date;
     createdBy: string;
     modifiedAt: Date;
     modifiedBy: string;
   };
-  roomInfo: {
-    id: number;
-    address: {
-      city: string;
-      oldAddress: string;
-      roadAddress: string;
-      detailAddress?: string;
-      stationName: string;
-      stationTime: number;
-      busStopTime: number;
-      schoolName: string;
-      schoolTime: number;
-      convenienceStortTime: number;
-    };
-    roomType: RoomType;
-    size: number;
-    numberOfRoom: number;
-    rentalType: RentalType;
-    price: number;
-    managementFee: number;
-    expectedPayment: number;
-    monthlyFee: number;
-  };
-  isScrapped: boolean;
-  scrapCount: number;
-  createdAt: Date;
-  createdBy: string;
-  modifiedAt: Date;
-  modifiedBy: string;
 }
