0618042f38145335e0d9193cc236053d332a9bb4
cjeongmin <cjeongmin27@gmail.com>
2024-04-13 20:40:09 +0900

    fix: DealTypeFilter (#50)

--- src/components/shared-posts/filter/DealTypeFilter.tsx
+++ src/components/shared-posts/filter/DealTypeFilter.tsx
@@ -8,19 +8,22 @@ const styles = {
   container: styled.div`
     display: flex;
     flex-direction: column;
-    gap: 1.56rem;
+    align-items: flex-start;
 
-    color: #000;
-    font-family: 'Noto Sans KR';
-    font-size: 1rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
+    gap: 2rem;
+    border-radius: 1.25rem;
+    background: #fff;
 
-    width: 30dvw;
-    height: 30dvw;
+    width: 20rem;
+  `,
+  item: styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 2rem;
 
-    h3 {
+    h1 {
       color: #000;
       font-family: 'Noto Sans KR';
       font-size: 1.125rem;
@@ -28,16 +31,50 @@ const styles = {
       font-weight: 500;
       line-height: normal;
     }
+
+    div {
+      display: flex;
+      align-items: flex-start;
+      gap: 0.5rem;
+
+      button {
+        all: unset;
+        cursor: pointer;
+
+        display: flex;
+        padding: 0.75rem 1.25rem;
+        justify-content: center;
+        align-items: center;
+        gap: 0.5rem;
+
+        border-radius: 0.5rem;
+        border: 1px solid #000;
+
+        color: #000;
+        font-family: 'Noto Sans KR';
+        font-size: 1rem;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+      }
+    }
   `,
 };
 
 export function DealTypeFilter() {
   return (
     <styles.container>
-      <h3>거래방식</h3>
-      <h3>보증금(전세금)</h3>
-      <RangeSlider min={0} max={100} onChange={() => {}} />
-      <h3>월세</h3>
+      <styles.item>
+        <h1>거래 방식</h1>
+        <div>
+          <button type="button">전세</button>
+          <button type="button">월세</button>
+        </div>
+      </styles.item>
+      <styles.item>
+        <h1>희망 월 분담금</h1>
+        <RangeSlider min={0} max={100} onChange={() => {}} />
+      </styles.item>
     </styles.container>
   );
 }
