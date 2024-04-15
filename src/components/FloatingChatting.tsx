f8d5da7e5acf687f5be028e7dd2f159ab7995545
he2e2 <dddd6368@naver.com>
2024-04-15 16:30:37 +0900

    feat: chatting-component(#52)

--- src/components/FloatingChatting.tsx
+++ src/components/FloatingChatting.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import Image from 'next/image';
 import React, { useState } from 'react';
 import styled from 'styled-components';
+
+import { ChattingList } from './chat/ChattingList';
 
 const styles = {
   chattingButton: styled.div`
@@ -24,10 +25,15 @@ const styles = {
       transform: scale(1.1);
     }
   `,
-  chattingContainer: styled.div`
+  buttonIcon: styled.div`
+    width: 2.60419rem;
+    height: 2.60419rem;
+    background: url('chatting.svg') no-repeat center;
+  `,
+  container: styled.div`
     position: fixed;
-    bottom: 5rem;
-    right: 6.5rem;
+    bottom: 6rem;
+    right: 7.5rem;
     display: flex;
     width: 25%;
     height: 70%;
@@ -38,16 +44,34 @@ const styles = {
     background: #fff;
     box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.2);
     z-index: 100;
-    overflow: 'auto';
     -webkit-transition: 0.4s;
     transition: 0.4s;
+    overflow: hidden;
   `,
   chattingHeader: styled.div`
+    display: inline-flex;
+    align-items: center;
+    padding-left: 1rem;
+    gap: 0.3rem;
     width: 100%;
     height: 3.25rem;
     flex-shrink: 0;
     border-radius: 20px 20px 0 0;
     background: var(--background, #f7f6f9);
+  `,
+  title: styled.span`
+    font-family: 'Baloo 2';
+    font-size: 1.575rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  `,
+  chattingSection: styled.div`
+    width: 100%;
+    height: calc(100% - 3.25rem);
+    display: flex;
+    flex-direction: column;
+    overflow-y: hidden;
   `,
 };
 
@@ -60,12 +84,25 @@ export function FloatingChatting() {
   return (
     <>
       <styles.chattingButton onClick={toggleChat}>
-        <Image src="/chatting.svg" width={25} height={25} alt="chatting-icon" />
+        <styles.buttonIcon />
       </styles.chattingButton>
       {isChatOpen && (
-        <styles.chattingContainer>
-          <styles.chattingHeader />
-        </styles.chattingContainer>
+        <styles.container>
+          <styles.chattingHeader>
+            <styles.title style={{ color: 'var(--Main-1, #E15637)' }}>
+              maru{' '}
+            </styles.title>
+            <styles.title style={{ color: 'var(--Gray, #8C95A8)' }}>
+              chat
+            </styles.title>
+          </styles.chattingHeader>
+          <styles.chattingSection>
+            <ChattingList />
+            <ChattingList />
+            <ChattingList />
+            <ChattingList />
+          </styles.chattingSection>
+        </styles.container>
       )}
     </>
   );
