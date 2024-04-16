4c0f206bdab2080252d0774f94ebec4e41556077
cjeongmin <cjeongmin27@gmail.com>
2024-04-16 19:37:33 +0900

    feat: hide the Geocoding API secret value using Route Handlers (#55)

--- src/features/auth/auth.api.ts
+++ src/features/auth/auth.api.ts
@@ -4,19 +4,19 @@ import { type PostTokenRefreshDTO, type GetUserDataDTO } from './auth.dto';
 
 export const getUserData = async () =>
   await axios
-    .get<GetUserDataDTO>('/api/auth/initial/info')
+    .get<GetUserDataDTO>('/maru-api/auth/initial/info')
     .then(response => response.data);
 
 export const postTokenRefresh = async (refreshToken: string) =>
   await axios
     .post<PostTokenRefreshDTO>(
-      '/api/auth/token/refresh',
+      '/maru-api/auth/token/refresh',
       {},
       { headers: { Authorization: `Bearer ${refreshToken}` } },
     )
     .then(response => response.data);
 
 export const getAuthLogout = async (refreshToken: string) =>
-  await axios.get('/api/auth/logout', {
+  await axios.get('/maru-api/auth/logout', {
     headers: { Authorization: `Bearer ${refreshToken}` },
   });
--- src/features/geocoding/geocoding.api.ts
+++ src/features/geocoding/geocoding.api.ts
@@ -3,12 +3,7 @@ import axios from 'axios';
 import { type FromAddrToCoordDTO } from '@/features/geocoding/geocoding.type';
 
 export const fromAddrToCoord = async ({ query }: { query: string }) =>
-  await axios.get<FromAddrToCoordDTO>(`/naveropenapi/geocode?query=${query}`, {
-    headers: {
-      'X-NCP-APIGW-API-KEY-ID': process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID,
-      'X-NCP-APIGW-API-KEY': process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_SECRET,
-    },
-  });
+  await axios.get<FromAddrToCoordDTO>(`/api/geocode/${query}`);
 
 export const getGeolocation = ({
   onSuccess,
