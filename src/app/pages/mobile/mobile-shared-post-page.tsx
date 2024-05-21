39c8a210d896f49dd826c1c4db50c49da90ed24f
cjeongmin <cjeongmin27@gmail.com>
2024-05-21 23:05:40 +0900

    feat: Apply data refetching for follow, unfollow, and scrap actions in mobile

--- src/app/pages/mobile/mobile-shared-post-page.tsx
+++ src/app/pages/mobile/mobile-shared-post-page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import styled from 'styled-components';
@@ -531,6 +532,8 @@ export function MobileSharedPostPage({
     setFollowList(newFollowList);
   }, [post]);
 
+  const queryClient = useQueryClient();
+
   if (isLoading || post == null) return <></>;
 
   return (
@@ -565,8 +568,40 @@ export function MobileSharedPostPage({
                           followList[selected.memberId] == null
                         )
                           return;
-                        if (followList[selected.memberId]) unfollow();
-                        else follow();
+                        if (followList[selected.memberId])
+                          unfollow(undefined, {
+                            onSuccess: () => {
+                              if (type === 'hasRoom')
+                                queryClient.invalidateQueries({
+                                  queryKey: [
+                                    `/api/shared/posts/studio/${postId}`,
+                                  ],
+                                });
+                              else
+                                queryClient.invalidateQueries({
+                                  queryKey: [
+                                    `/api/shared/posts/dormitory/${postId}`,
+                                  ],
+                                });
+                            },
+                          });
+                        else
+                          follow(undefined, {
+                            onSuccess: () => {
+                              if (type === 'hasRoom')
+                                queryClient.invalidateQueries({
+                                  queryKey: [
+                                    `/api/shared/posts/studio/${postId}`,
+                                  ],
+                                });
+                              else
+                                queryClient.invalidateQueries({
+                                  queryKey: [
+                                    `/api/shared/posts/dormitory/${postId}`,
+                                  ],
+                                });
+                            },
+                          });
                       }}
                       hasBorder
                       color="#888"
@@ -606,7 +641,18 @@ export function MobileSharedPostPage({
                 hasBorder={false}
                 marked={post.data.isScrapped}
                 onToggle={() => {
-                  scrapPost(postId);
+                  scrapPost(postId, {
+                    onSuccess: () => {
+                      if (type === 'hasRoom')
+                        queryClient.invalidateQueries({
+                          queryKey: [`/api/shared/posts/studio/${postId}`],
+                        });
+                      else
+                        queryClient.invalidateQueries({
+                          queryKey: [`/api/shared/posts/dormitory/${postId}`],
+                        });
+                    },
+                  });
                 }}
                 color="black"
               />
